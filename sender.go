dd29e982b08a99cbf3c5e956ab7c2b7b7fb7d575
Nicolás Maldonado <nicolas.maldonado.13@sansano.usm.cl>
2021-11-04 19:51:43 -0300

    🚧 chango to sender

--- sender.go
+++ sender.go
@@ -2,7 +2,6 @@ package main
 
 import (
   "log"
-
   "github.com/streadway/amqp"
 )
 
@@ -12,33 +11,36 @@ func failOnError(err error, msg string) {
   }
 }
 
-conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-failOnError(err, "Failed to connect to RabbitMQ")
-defer conn.Close()
+func main() {
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
 
 
-ch, err := conn.Channel()
-failOnError(err, "Failed to open a channel")
-defer ch.Close()
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
 
-q, err := ch.QueueDeclare(
-  "hello", // name
-  false,   // durable
-  false,   // delete when unused
-  false,   // exclusive
-  false,   // no-wait
-  nil,     // arguments
-)
-failOnError(err, "Failed to declare a queue")
+	q, err := ch.QueueDeclare(
+	"hello", // name
+	false,   // durable
+	false,   // delete when unused
+	false,   // exclusive
+	false,   // no-wait
+	nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
 
-body := "Hello World!"
-err = ch.Publish(
-  "",     // exchange
-  q.Name, // routing key
-  false,  // mandatory
-  false,  // immediate
-  amqp.Publishing {
-    ContentType: "text/plain",
-    Body:        []byte(body),
-  })
-failOnError(err, "Failed to publish a message")
+	body := "Hello World!"
+	err = ch.Publish(
+	"",     // exchange
+	q.Name, // routing key
+	false,  // mandatory
+	false,  // immediate
+	amqp.Publishing {
+		ContentType: "text/plain",
+		Body:        []byte(body),
+	})
+	failOnError(err, "Failed to publish a message")
+}
