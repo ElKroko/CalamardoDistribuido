b1eeabf681b60ed18fcd8c2e2f127d2978a90fe6
Nicolás Maldonado <nicolas.maldonado.13@sansano.usm.cl>
2021-11-04 19:53:55 -0300

    🚧 Change to receiver

--- receiver.go
+++ receiver.go
@@ -12,41 +12,44 @@ func failOnError(err error, msg string) {
   }
 }
 
-conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-failOnError(err, "Failed to connect to RabbitMQ")
-defer conn.Close()
+func main() {
 
-ch, err := conn.Channel()
-failOnError(err, "Failed to open a channel")
-defer ch.Close()
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
 
-q, err := ch.QueueDeclare(
-  "hello", // name
-  false,   // durable
-  false,   // delete when unused
-  false,   // exclusive
-  false,   // no-wait
-  nil,     // arguments
-)
-failOnError(err, "Failed to declare a queue")
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
 
-msgs, err := ch.Consume(
-	q.Name, // queue
-	"",     // consumer
-	true,   // auto-ack
-	false,  // exclusive
-	false,  // no-local
-	false,  // no-wait
-	nil,    // args
-  )
-  failOnError(err, "Failed to register a consumer")
-  
-  forever := make(chan bool)
-  
-  go func() {
-	for d := range msgs {
-	  log.Printf("Received a message: %s", d.Body)
-	}
-  }()
-  
-  log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	q, err := ch.QueueDeclare(
+	"hello", // name
+	false,   // durable
+	false,   // delete when unused
+	false,   // exclusive
+	false,   // no-wait
+	nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+	
+	forever := make(chan bool)
+	
+	go func() {
+		for d := range msgs {
+		log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+	
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+}
