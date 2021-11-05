7a0e964ecd18d55ed545803dd9e7b616a456c600
Vicente Perelli <vicente.perelli@sansano.usm.cl>
2021-11-05 02:13:15 -0300

    abriendo puertos

--- grpc/client/main.go
+++ grpc/client/main.go
@@ -16,7 +16,7 @@ func generateID() string {
 }
 
 func main() {
-	conn, err := grpc.Dial("10.6.43.109:50051", grpc.WithInsecure())
+	conn, err := grpc.Dial("10.6.43.109:8080", grpc.WithInsecure())
 
 	if err != nil {
 		panic("cannot connect with server " + err.Error())
