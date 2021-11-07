6958f9b733ad5e2d44495bd03f37c950206dc92f
Nicolás Maldonado <nicolas.maldonado.13@sansano.usm.cl>
2021-11-07 14:26:50 -0300

    Prueba RabittMQ

--- grpc/juego/sender.go
+++ grpc/juego/sender.go
@@ -17,30 +17,34 @@ func main() {
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
-
 	ch, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(
-	"hello", // name
-	false,   // durable
-	false,   // delete when unused
-	false,   // exclusive
-	false,   // no-wait
-	nil,     // arguments
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
 
-	body := "Hello World!"
+	i := "1"
+	s := "2"
+
+	body := "Jugador_" + i + " Ronda_" + s
+
 	err = ch.Publish(
-	"",     // exchange
-	q.Name, // routing key
-	false,  // mandatory
-	false,  // immediate
-	amqp.Publishing {
-		ContentType: "text/plain",
-		Body:        []byte(body),
-	})
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
 	failOnError(err, "Failed to publish a message")
+	log.Printf("Ha muerto: %s ", body)
 }
