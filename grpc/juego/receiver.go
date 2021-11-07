55d8ae059fc75d492fb3543c8321152126164ebc
Nicolás Maldonado <nicolas.maldonado.13@sansano.usm.cl>
2021-11-07 12:40:19 -0300

    Prueba RabbitMQ

--- grpc/juego/receiver.go
+++ grpc/juego/receiver.go
@@ -13,7 +13,7 @@ func failOnError(err error, msg string) {
 
 func main() {
 
-	conn, err := amqp.Dial("qa1://admin:test@10.6.43.111:5672/")
+	conn, err := amqp.Dial("amqp://admin:test@10.6.43.111:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
